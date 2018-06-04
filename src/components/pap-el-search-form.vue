cd0244aaac7dba8e0cf16c556e19956abf4417cd
alexgaoyh <alexgaoyh@sina.com>
2018-06-04 17:27:02 +0800

    下拉框属性添加.

--- src/components/pap-el-search-form.vue
+++ src/components/pap-el-search-form.vue
@@ -10,8 +10,9 @@
                       :disabled="form.disabled"
                       :placeholder="form.placeholder"
                       :style="itemStyle + (form.itemWidth ? `width: ${form.itemWidth}px;` : '')" ></el-input>
-            <v-select v-if="form.type == 'select'"v-model="form.value"
+·            <v-select v-if="form.type == 'select'" v-model="form.value"
                        :size="form.size ? form.size : size" :options="form.options"
+                       :label="form.optionLabel" :value="form.optionValue"
                        :readonly="form.readonly"
                        :disabled="form.disabled"
                        :placeholder="form.placeholder"
