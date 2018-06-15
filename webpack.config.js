4274e87f4dc510ee85f5bace035b77c56d449e33
alexgaoyh <alexgaoyh@sina.com>
2018-06-15 15:24:55 +0800

    页面保存.

--- webpack.config.js
+++ webpack.config.js
@@ -52,14 +52,14 @@ webpackConfig.externals = {
 }
 module.exports = [
     merge(webpackConfig, {
-        entry: path.resolve('./src/pap-base-table.js'),
+        entry: path.resolve('./src/pap-rbac-user.js'),
         devServer: {
             contentBase: path.join(__dirname, "dist")
         },
         output: {
-            filename: 'pap-base-table.min.js',
+            filename: 'pap-rbac-user.min.js',
             libraryTarget: 'window',
-            library: 'PapBaseTable',
+            library: 'PapRbacUser',
         }
     })
 ]
